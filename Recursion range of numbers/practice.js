607929f68daf6bb47f32603026d216cc86ec49bc
Noémie Mbarga <noemielck@gmail.com>
2022-08-30 16:09:57 +0100

    default parametres for your function

--- Recursion range of numbers/practice.js
+++ Recursion range of numbers/practice.js
@@ -15,3 +15,8 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2); 
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// set default parametres for your function
+
+const increment = (number, value = 1) => number + value;
+console.log(increment());
