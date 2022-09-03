7bdac81484d4914034db8e1264b00ff29fb899a4
Noémie Mbarga <noemielck@gmail.com>
2022-09-03 11:14:54 +0100

    gain calculator

---
+++ gain calculator/calculate.js
@@ -0,0 +1,17 @@
+function calculateBill(bill){
+    
+    if (bill => 50){
+        const gain = (bill * 15/100);
+        console.log(gain());
+    }else if (bill <= 300){
+        const gain = (bill * 15/100);
+        console.log(gain());
+
+    }else {
+        const gain = (bill * 20/100);
+        console.log(gain());
+    }
+
+    const bill =[125, 555, 44];
+
+}
