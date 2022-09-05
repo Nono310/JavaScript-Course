6d3eb4bc9dad2d50efe481c13e1ed39106143f3c
Noémie Mbarga <noemielck@gmail.com>
2022-09-05 14:44:07 +0100

    passing arguments to resolve and reject methods

--- Recursion range of numbers/practice.js
+++ Recursion range of numbers/practice.js
@@ -175,3 +175,17 @@ subtract(7,4);
 const makeServerRequest = new Promise((resolve, reject) => {
 
 });
+
+// complete a promise with resolve and reject 
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    // Change this line
+    resolve('We got the data');
+  } else {  
+    // Change this line
+    reject('Data not received');
+  }
+});
