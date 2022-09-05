4a0e9dd69ef75d9f7992eeeb9a8f471739ed5bbd
Noémie Mbarga <noemielck@gmail.com>
2022-09-05 10:38:18 +0100

    passing an object as a function using destructuring

--- Recursion range of numbers/practice.js
+++ Recursion range of numbers/practice.js
@@ -53,3 +53,18 @@ const HIGH_TEMPERATURES = {
   
   // Only change code below this line
     const{ today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+    
+    // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+    const stats = {
+      max: 56.78,
+      standard_deviation: 4.34,
+      median: 34.54,
+      mode: 23.87,
+      min: -0.75,
+      average: 35.85
+    };
+    
+    // Only change code below this line
+    const half = ({max , min}) => (max + min) / 2.0; 
+    console.log(half(stats));
+    // Only change code above this line
